a9b7d9df6f1b09a1716650492552aa0c8b2f85a3
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-01-28 21:44:14 +0100

    updated todos

--- include/gfx/renderer.h
+++ include/gfx/renderer.h
@@ -12,6 +12,7 @@
 #include <gfx/color.h>
 #include <gfx/font.h>
 
+// TODO: add thick line rendering
 // TODO: draw circle/rect outline
 // TODO: make pkg-config file work
 // TODO: fix library initializer check segfault in gfx::Texture (gl::Texture is constructed before the check)
