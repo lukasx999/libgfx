64d10a5692f64c5831085eb2f34e3e57364cc077
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-01-29 16:04:50 +0100

    explicitly removed font copy ctor/assignment op

--- src/font_impl.h
+++ src/font_impl.h
@@ -10,9 +10,14 @@ namespace gfx {
 
 struct Font::Impl {
     FT_Face m_face;
-    [[nodiscard]] Glyph load_glyph(char c, unsigned int size) const;
+
     Impl(FT_Library ft, const char* path);
     ~Impl();
+    Impl(const Impl&) = delete;
+    Impl& operator=(const Impl&) = delete;
+
+    [[nodiscard]] Glyph load_glyph(char c, unsigned int size) const;
+
 };
 
 } // namespace gfx
