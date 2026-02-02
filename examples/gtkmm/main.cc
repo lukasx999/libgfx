ee73785bcd98f71a227b782a70ca6f3f520c1c8a
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-02-02 22:18:19 +0100

    updated gtkmm example

--- examples/gtkmm/main.cc
+++ examples/gtkmm/main.cc
@@ -1,64 +1,83 @@
-#include <print>
 #include <functional>
 
 #include <gtkmm.h>
 #include <gfx/gfx.h>
 
-#include <epoxy/gl.h>
-
-class MyWindow : public Gtk::Window {
-
-    Gtk::GLArea m_glarea;
-    Gtk::Button m_button{"Hello, World!"};
-    Gtk::Box m_box{Gtk::Orientation::VERTICAL, 0};
+class GfxArea : public Gtk::Frame {
 
     // using std::optional to defer construction of gfx context until
     // gktmm has initialized its opengl context.
     std::optional<gfx::ExternalContext> m_context;
+
+    Gtk::GLArea m_area;
+
+    using SignalRender = sigc::signal<void(gfx::Renderer&)>;
+    SignalRender m_signal_render;
+
+public:
+    GfxArea() {
+        m_area.set_required_version(4, 6);
+        m_area.set_expand(true);
+        m_area.set_auto_render(true);
+
+        m_area.signal_resize().connect(sigc::mem_fun(*this, &GfxArea::on_gl_resize));
+        m_area.signal_realize().connect(sigc::mem_fun(*this, &GfxArea::on_gl_realize));
+        m_area.signal_unrealize().connect(sigc::mem_fun(*this, &GfxArea::on_gl_unrealize), false);
+        m_area.signal_render().connect(sigc::mem_fun(*this, &GfxArea::on_gl_render), false);
+
+        set_child(m_area);
+    }
+
+    [[nodiscard]] SignalRender& signal_on_render() {
+        return m_signal_render;
+    }
+
+private:
+    void on_gl_resize(int width, int height) {
+        m_context.emplace(width, height);
+    }
+
+    bool on_gl_render([[maybe_unused]] const Glib::RefPtr<Gdk::GLContext>& context) {
+
+        m_context->draw([&](gfx::Renderer& rd) {
+            m_signal_render.emit(rd);
+        });
+
+        return true;
+    }
+
+    void on_gl_realize() {
+        m_area.make_current();
+    }
+
+    void on_gl_unrealize() {
+        m_context.reset();
+    }
+
+};
+
+class MyWindow : public Gtk::Window {
+
+    Gtk::Box m_box{Gtk::Orientation::VERTICAL, 0};
+    GfxArea m_area;
+    Gtk::Button m_button{"Hello, World!"};
 
 public:
     MyWindow() {
         set_title("gtkmm example");
         set_default_size(1600, 900);
 
-        m_glarea.set_required_version(4, 6);
-        m_glarea.set_expand(true);
-        m_glarea.set_auto_render(true);
+        m_area.signal_on_render().connect(sigc::mem_fun(*this, &MyWindow::draw));
 
         set_child(m_box);
-        m_box.append(m_glarea);
+        m_box.append(m_area);
         m_box.append(m_button);
-
-        m_glarea.signal_resize().connect(sigc::mem_fun(*this, &MyWindow::on_gl_resize));
-        m_glarea.signal_realize().connect(sigc::mem_fun(*this, &MyWindow::on_gl_realize));
-        m_glarea.signal_unrealize().connect(sigc::mem_fun(*this, &MyWindow::on_gl_unrealize), false);
-        m_glarea.signal_render().connect(sigc::mem_fun(*this, &MyWindow::on_gl_render), false);
-
     }
 
 private:
-    static void gfx_draw(gfx::Renderer& rd) {
+    void draw(gfx::Renderer& rd) const {
         rd.clear_background(gfx::Color::white());
         rd.draw_circle(rd.get_surface().get_center(), 100, gfx::Color::blue());
-    }
-
-    void on_gl_resize(int width, int height) {
-        m_context.emplace(width, height);
-    }
-
-    bool on_gl_render([[maybe_unused]] const Glib::RefPtr<Gdk::GLContext>& context) {
-        m_glarea.throw_if_error();
-        m_context->draw(gfx_draw);
-        return true;
-    }
-
-    void on_gl_realize() {
-        m_glarea.throw_if_error();
-        m_glarea.make_current();
-    }
-
-    void on_gl_unrealize() {
-        m_context.reset();
     }
 
 };
