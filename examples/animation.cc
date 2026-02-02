c2ac815b0beff8ba4bf85ab1e354e32a27865dc7
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-02-02 20:18:22 +0100

    added proper animation example

--- examples/animation.cc
+++ examples/animation.cc
@@ -1,18 +1,26 @@
 #include <gfx/gfx.h>
 
-
 int main() {
 
     using namespace std::chrono_literals;
 
-    gfx::Window window(1600, 900, "animation");
+    gfx::Window window(700, 700, "animation");
 
-    gfx::Animation<float> radius(50.0f, 300.0f, 3s, gfx::interpolators::ease_in_quad);
+    gfx::Animation<gfx::Vec> pos({ 0.0f, 0.0f }, window.get_center(), 2s, gfx::interpolators::ease_in_out_cubic);
+    gfx::Animation<float> radius(0, 100, 2s);
+    gfx::Animation<gfx::Color> color(gfx::Color::red(), gfx::Color::blue(), 5s);
+    gfx::Animation<gfx::Rect> rect({ 300, 300, 100, 100 }, window.get_as_rect(), 1s, gfx::interpolators::ease_in_expo);
+
+    pos.start();
     radius.start();
+    color.start();
+    rect.start();
 
     window.draw_loop([&](gfx::Renderer& rd) {
         rd.clear_background(gfx::Color::black());
-        rd.draw_circle(window.get_center(), radius, gfx::Color::white());
+
+        rd.draw_rectangle(rect, gfx::Color::gray());
+        rd.draw_circle(pos, radius, color);
 
         if (window.get_key_state(gfx::Key::Escape).pressed())
             window.close();
