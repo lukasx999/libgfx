01b5960237668e5dc84481cde5818605c3863351
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-02-03 20:42:56 +0100

    added comment to view matrix function

--- src/renderer_impl.cc
+++ src/renderer_impl.cc
@@ -8,21 +8,19 @@ namespace gfx {
 glm::mat4 Renderer::Impl::gen_view_matrix(const gfx::Surface& surface, gfx::Vec center, gfx::Rotation rotation) {
     gfx::Vec screen_center = surface.get_center();
 
-    glm::vec3 camera_position(
-        center.x - screen_center.x,
-        center.y - screen_center.y,
-        0.0f
-    );
-
-    glm::vec3 camera_direction(0.0f, 0.0f, -1.0f);
+    // the camera position used in the lookAt() function has its origin at (0,0), but
+    // the origin of the viewer is the center of the screen, so we have to account for that
+    // by subtracting the center coordinates
+    glm::vec3 position(center.x - screen_center.x, center.y - screen_center.y, 0.0f);
+    glm::vec3 direction(0.0f, 0.0f, -1.0f);
     glm::vec3 up(0.0f, 1.0f, 0.0f);
 
-    glm::mat4 view = glm::lookAt(camera_position, camera_position+camera_direction, up);
+    glm::mat4 view = glm::lookAt(position, position+direction, up);
 
     glm::mat4 transform(1.0f);
-    transform = glm::translate(transform, glm::vec3(screen_center.x, screen_center.y, 0.0));
+    transform = glm::translate(transform, glm::vec3(screen_center.x, screen_center.y, 0.0f));
     transform = glm::rotate(transform, rotation.get_radians(), glm::vec3(0.0f, 0.0f, 1.0f));
-    transform = glm::translate(transform, glm::vec3(-screen_center.x, -screen_center.y, 0.0));
+    transform = glm::translate(transform, glm::vec3(-screen_center.x, -screen_center.y, 0.0f));
     view = transform * view;
 
     return view;
