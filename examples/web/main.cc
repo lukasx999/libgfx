3819ba41b86696ac2253b7961880e4cf23ab01db
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-02-09 16:54:43 +0100

    added proper web example

--- examples/web/main.cc
+++ examples/web/main.cc
@@ -1,3 +1,5 @@
+#include <print>
+
 #include <emscripten.h>
 #include <gfx/gfx.h>
 
@@ -5,13 +7,13 @@ void loop(void* arg) {
     gfx::Window& window = *static_cast<gfx::Window*>(arg);
 
     window.with_draw_loop_context([&](gfx::Renderer& rd) {
-        rd.clear_background(gfx::Color::blue());
-        rd.draw_circle(window.get_center(), 100, gfx::Color::red());
+        rd.clear_background(gfx::Color::black());
+        rd.draw_circle(window.get_center(), 100, gfx::Color::blue());
     });
 
 }
 
 int main() {
-    gfx::Window window(500, 500, "example");
+    gfx::Window window(500, 500, "foo");
     emscripten_set_main_loop_arg(loop, &window, 0, true);
 }
