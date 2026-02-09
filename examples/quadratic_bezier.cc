2cd470401ec2c14a123536c342bcfec09bde1eae
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-02-09 17:43:07 +0100

    added movable handles to bezier curve example

--- examples/quadratic_bezier.cc
+++ examples/quadratic_bezier.cc
@@ -1,21 +1,60 @@
+#include <print>
+
 #include <gfx/gfx.h>
 
 int main() {
 
     gfx::Window window(700, 700, "quadratic bezier curve");
 
+    float handle_radius = 10;
+
+    bool moving_a = false;
+    bool moving_b = false;
+    bool moving_ctl = false;
+
+    gfx::Vec a(100, 500);
+    gfx::Vec b(600, 500);
+    gfx::Vec ctl(350, 100);
+
     window.draw_loop([&](gfx::Renderer& rd) {
         rd.clear_background(gfx::Color::black());
 
-        gfx::Vec a(100, 500);
-        gfx::Vec b(600, 500);
-        auto ctl = window.get_mouse_pos();
+        auto mouse = window.get_mouse_pos();
 
-        rd.draw_circle(a, 5, gfx::Color::white());
-        rd.draw_circle(b, 5, gfx::Color::white());
-        rd.draw_circle(ctl, 5, gfx::Color::red());
+        if (window.get_mouse_button_state(gfx::MouseButton::Left).pressed()) {
+            if (mouse.distance(a) <= handle_radius)
+                moving_a = true;
 
-        rd.draw_quadratic_bezier_curve(a, b, ctl, gfx::Color::white());
+            if (mouse.distance(b) <= handle_radius)
+                moving_b = true;
+
+            if (mouse.distance(ctl) <= handle_radius)
+                moving_ctl = true;
+
+        }
+
+        if (moving_a)
+            a = mouse;
+
+        if (moving_b)
+            b = mouse;
+
+        if (moving_ctl)
+            ctl = mouse;
+
+        if (window.get_mouse_button_state(gfx::MouseButton::Left).released()) {
+            moving_a = false;
+            moving_b = false;
+            moving_ctl = false;
+        }
+
+
+
+        rd.draw_quadratic_bezier_curve(a, b, ctl, 5, gfx::Color::white());
+
+        rd.draw_circle(a, handle_radius, gfx::Color::blue());
+        rd.draw_circle(b, handle_radius, gfx::Color::blue());
+        rd.draw_circle(ctl, handle_radius, gfx::Color::red());
 
         if (window.get_key_state(gfx::Key::Escape).pressed())
             window.close();
